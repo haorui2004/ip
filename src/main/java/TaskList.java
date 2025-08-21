469cf29fb3f8be21ef75e2e2509d42f8af8bc095
hao rui <194741615+haorui2004@users.noreply.github.com>
2025-08-21 22:32:41 +0800

    Added the TaskList class

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    protected ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task t) {
+        tasks.add(t);
+    }
+
+    public void mark(int number) {
+        Task t = tasks.get(number - 1);
+        t.markDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.print("  " + t + "\n");
+    }
+
+    public void unmark(int number) {
+        Task t = tasks.get(number - 1);
+        t.unmarkDone();
+        System.out.println("Ok, I've marked this task as not done yet:");
+        System.out.print("  " + t + "\n");
+    }
+
+    public void showList() {
+        if(!tasks.isEmpty()) {
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println((i + 1) + ". " + tasks.get(i));
+            }
+        }
+    }
+}
