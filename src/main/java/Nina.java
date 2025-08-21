2ebb028e7f1047493d0c962a2f483d7a7ae9addb
hao rui <194741615+haorui2004@users.noreply.github.com>
2025-08-21 20:46:09 +0800

    Allow tasks to be added to a list and print full task list

--- src/main/java/Nina.java
+++ src/main/java/Nina.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Nina {
     static String line = "___________________________________\n";
+    static ArrayList<String> tasks = new ArrayList<>();
 
     public static void greet() {
+        System.out.print(line);
         System.out.println("Hello, I am your personal assistant Nina\n" +
                 "What can I do for you?");
         System.out.print(line);
     }
 
     public static void exit() {
+        System.out.print(line);
         System.out.println("See you again soon!");
         System.out.print(line);
     }
@@ -18,11 +22,25 @@ public class Nina {
         Scanner sc = new Scanner(System.in);
         while(sc.hasNextLine()) {
             String command = sc.nextLine();
+
+            if(command.equals("list")) {
+                System.out.print(line);
+                for(int i =0; i< tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+                System.out.print(line);
+                continue;
+            }
+
+            tasks.add(command);
+
             if (command.equals("bye")) {
                 exit();
                 break;
             }
-            System.out.println(command);
+
+            System.out.print(line);
+            System.out.println("added: "+ command);
             System.out.print(line);
         }
     }
